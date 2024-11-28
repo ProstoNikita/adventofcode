d59349e19152972e7664c8e09d9430071e735829
Nikita Iryupin <nikita.iryupin@scalesoft.cz>
2024-11-28 14:19:06 +0100

    Finished 2015/02, starting 2015/03

--- 2015/Day02/Solution.cs
+++ 2015/Day02/Solution.cs
@@ -9,12 +9,31 @@ using System.Text;
 
 [ProblemName("I Was Told There Would Be No Math")]
 class Solution : Solver {
-
     public object PartOne(string input) {
-        return 0;
+        return Solve(input);
     }
 
     public object PartTwo(string input) {
-        return 0;
+        return Solve2(input);
+    }
+
+    private int Solve(string input) {
+        return input.Trim().Split("\n").Select(x => x.Split("x").Select(int.Parse)).Select(SolveSingular).Aggregate((x, y) => x + y);
+    }
+
+    private int Solve2(string input) {
+        return input.Trim().Split("\n").Select(x => x.Split("x").Select(int.Parse)).Select(SolveSingular2).Aggregate((x, y) => x + y);
+    }
+
+    private int SolveSingular(IEnumerable<int> input) {
+        var list = input.ToList();
+        list.Sort();
+        return 3 * list[0] * list[1] + 2 * list[2] * list[1] + 2 * list[0] * list[2];
+    }
+
+    private int SolveSingular2(IEnumerable<int> input) {
+        var list = input.ToList();
+        list.Sort();
+        return 2 * list[0] + 2 * list[1] + list[0] * list[1] * list[2];
     }
 }
